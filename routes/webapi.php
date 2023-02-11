b6b71af8e2eb27cd4ee20375f5de2edad7821e30
Tech Cafe <techcafe13@gmail.com>
2023-02-12 02:15:14 +0530

    user

--- routes/webapi.php
+++ routes/webapi.php
@@ -21,30 +21,4 @@ Route::get('/', function(){
 // USER API
 Route::post('/register', [UserController::class,'register']);
 Route::post('/login', [UserController::class,'login']);
-
-
 Route::middleware('auth:sanctum')->post('/user', [UserController::class, 'getUser']);
-
-// PRODUCT API
-// Route::post('/products', ProductController::class);
-
-// const config = {
-//     headers: { Authorization: `Bearer ${token}` }
-// };
-
-// const bodyParameters = {
-//    key: "value"
-// };
-
-// Axios.post( 
-//   'http://localhost:8000/api/v1/get_token_payloads',
-//   bodyParameters,
-//   config
-// ).then(console.log).catch(console.log);
-
-// let data = new FormData();
-// data.append('file', document.getElementById('file').files[0]);
-
-// axios.post('/Upload/File',data).then(function (response) {
-//     console.log(response.data);
-// });
